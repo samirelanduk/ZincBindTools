df112df8f80de024b9e263a6d323a3d6d14f2c76
Sam Ireland <sam.ireland.uk@gmail.com>
2019-11-08 00:05:58 +0000

    Use ZincBind API to count binding sites

--- src/js/zincbindtools-rcsb.js
+++ src/js/zincbindtools-rcsb.js
@@ -21,4 +21,11 @@ if (smallMolecules.includes("ZN")) {
     newItem.getElementsByTagName("strong").item(0).innerText = "ZincBind: "
     newItem.innerHTML = `<strong>ZincBind:</strong> <a href="https://zincbind.bioinf.org.uk/pdbs/${code}/">${code}</a>`
     list.appendChild(newItem);
+
+    // Find out how many zinc binding sites there are
+    fetch(`https://api.zincbind.net/?query=%7Bpdb(id%3A%20%22${code}%22)%20%7Bzincsites%20%7B%20count%20%7D%7D%7D`).then((resp) => resp.json()).then(function(data) {
+        var siteCount = data.data.pdb.zincsites.count;
+        newItem.innerHTML = `<strong>ZincBind:</strong> <a href="https://zincbind.bioinf.org.uk/pdbs/${code}/">${siteCount} zinc binding site${siteCount == 1 ? '' : 's'}</a>`
+    });
+    
 }
